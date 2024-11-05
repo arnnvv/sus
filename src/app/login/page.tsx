1d56244c705e9c800ef6c01ed8588ac3a2e1334f
arnnvv <arnav.900@protonmail.com>
2024-11-05 17:04:35 +0530

    real

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -45,7 +45,7 @@ export default function LoginPage(): JSX.Element {
 
               try {
                 const response = await fetch(
-                  "https://localhost:8443/device/deviceLogin",
+                  "https://ec2-35-154-80-88.ap-south-1.compute.amazonaws.com:8081/device/deviceLogin",
                   {
                     method: "POST",
                     headers: {
